82fd5a2d36a456d033e83cc4fc365e20aebfd2ad
newarnetd <newarnetdevs@gmail.com>
2024-11-13 16:14:44 +0200

    new

--- ecommerce_admin_app/lib/main.dart
+++ ecommerce_admin_app/lib/main.dart
@@ -16,60 +16,42 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-
     options: DefaultFirebaseOptions.currentPlatform,
-
-);
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) =>  AdminProvider(),
+      create: (context) => AdminProvider(),
       builder: (context, child) => MaterialApp(
         title: 'Ecommerce Admin App',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-      routes: {
-        "/": (context)=> CheckUser(),
-        "/login" :(context)=> LoginPage(),
-        "/signup" : (context)=> SingupPage(),
-        "/home" : (context)=> AdminHome(),
-        "/category" : (context)=> CategoriesPage(),
-        "/products": (context) => ProductsPage(),
-        "/add_product" : (context)=> ModifyProduct(),
-        "/view_product": (context)=> ViewProduct(),
-        "/promos": (context)=> PromoBannersPage(),
-        "/update_promo":(context)=> ModifyPromo(),
-        "/coupons": (context)=> CouponsPage(),
-        "/orders":(context)=> OrdersPage(),
-        "/view_order": (context)=> ViewOrder()
-      },
+        routes: {
+          "/": (context) => CheckUser(),
+          "/login": (context) => LoginPage(),
+          "/signup": (context) => SingupPage(),
+          "/home": (context) => AdminHome(),
+          "/category": (context) => CategoriesPage(),
+          "/products": (context) => ProductsPage(),
+          "/add_product": (context) => ModifyProduct(),
+          "/view_product": (context) => ViewProduct(),
+          "/promos": (context) => PromoBannersPage(),
+          "/update_promo": (context) => ModifyPromo(),
+          "/coupons": (context) => CouponsPage(),
+          "/orders": (context) => OrdersPage(),
+          "/view_order": (context) => ViewOrder()
+        },
       ),
     );
   }
@@ -83,7 +65,6 @@ class CheckUser extends StatefulWidget {
 }
 
 class _CheckUserState extends State<CheckUser> {
-
   @override
   void initState() {
     AuthService().isLoggedIn().then((value) {
@@ -98,6 +79,10 @@ class _CheckUserState extends State<CheckUser> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body:  Center(child: CircularProgressIndicator(),),);
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -19,19 +19,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:provider/provider.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
 
-);
-await dotenv.load(fileName: ".env");
-  Stripe.publishableKey=dotenv.env["STRIPE_PUBLISH_KEY"]!;
-   Stripe.merchantIdentifier = 'merchant.flutter.stripe.test';
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await dotenv.load(fileName: ".env");
+  Stripe.publishableKey = dotenv.env["STRIPE_PUBLISH_KEY"]!;
+  Stripe.merchantIdentifier = 'merchant.flutter.stripe.test';
   Stripe.urlScheme = 'flutterstripe';
   await Stripe.instance.applySettings();
-
 
   runApp(const MyApp());
 }
@@ -39,57 +37,42 @@ await dotenv.load(fileName: ".env");
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) =>  UserProvider(),),
-        ChangeNotifierProvider(create: (context) =>  CartProvider(),),
+        ChangeNotifierProvider(
+          create: (context) => UserProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CartProvider(),
+        ),
       ],
       child: MaterialApp(
         title: 'eCommerce App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
           useMaterial3: true,
         ),
         routes: {
-          "/":(context)=> CheckUser(),
-          "/login": (context)=> LoginPage(),
-          "/home": (context)=> HomeNav(),
-          "/signup": (context)=> SingupPage(),
-          "/update_profile":(context)=> UpdateProfile(),
-          "/discount": (context)=> DiscountPage(),
-          "/specific": (context)=> SpecificProducts(),
-          "/view_product":(context)=> ViewProduct(),
-          "/cart": (context)=> CartPage(),
-          "/checkout":(context)=> CheckoutPage(),
-          "/orders":(context)=> OrdersPage(),
-          "/view_order":(context)=> ViewOrder(),
+          "/": (context) => CheckUser(),
+          "/login": (context) => LoginPage(),
+          "/home": (context) => HomeNav(),
+          "/signup": (context) => SingupPage(),
+          "/update_profile": (context) => UpdateProfile(),
+          "/discount": (context) => DiscountPage(),
+          "/specific": (context) => SpecificProducts(),
+          "/view_product": (context) => ViewProduct(),
+          "/cart": (context) => CartPage(),
+          "/checkout": (context) => CheckoutPage(),
+          "/orders": (context) => OrdersPage(),
+          "/view_order": (context) => ViewOrder(),
         },
-       
       ),
     );
   }
 }
-
 
 class CheckUser extends StatefulWidget {
   const CheckUser({super.key});
@@ -99,7 +82,6 @@ class CheckUser extends StatefulWidget {
 }
 
 class _CheckUserState extends State<CheckUser> {
-
   @override
   void initState() {
     AuthService().isLoggedIn().then((value) {
@@ -114,6 +96,10 @@ class _CheckUserState extends State<CheckUser> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body:  Center(child: CircularProgressIndicator(),),);
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
